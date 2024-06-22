d7872b047c97155df8bb6294fb54570dc0c68550
Hema0324 <63283241+Hema0324@users.noreply.github.com>
2024-06-22 16:25:41 +0530

    Add files via upload

---
+++ Student.js
@@ -0,0 +1,12 @@
+class Student
+{
+    constructor(name)
+    {
+        this.studentName = name
+    }
+
+    show()
+    {
+        console.log("My name is "+""+this.studentName)
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,34 @@
+
+
+
+function setup()
+{
+createCanvas(100,200)
+
+student1 = new Student("Anshpreet");
+student1.show()
+
+student1 = new Student("A");
+student1.show()
+
+student1 = new Student("B");
+student1.show()
+
+student1 = new Student("C");
+student1.show()
+
+student1 = new Student("D");
+student1.show()
+
+student1 = new Student("E");
+student1.show()
+
+
+
+}
+
+function draw()
+{
+  
+
+}
